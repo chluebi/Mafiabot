c942a980499d7475dbb33ac9fd32934e761835d5
celloboiErnest <ernestl8502@gmal.com>
2019-01-02 11:56:37 -0800

    second

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import asyncio
+import json
+import os
+import random
+
+bot = commands.Bot(command_prefix='/')
+bot.remove_command('help')
+
+extensions = ['mafia']
+players = {}
+@bot.event
+async def on_ready():
+    await bot.change_presence(game = discord.Game(name="/helpM", type=1))
+    print("I'm in")
+  
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        await bot.process_commands(message)
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            if(extension == "mafia"):
+                bot.load_extension(f"MAFIA.{extension}")
+            else:
+                bot.load_extension(extension)
+        except Exception as error:
+            print('{} cannot be loaded because {}'.format(extension, error))
+    bot.run('NTExNzg2OTE4NzgzMDkwNjg4.DswJvw.pB_bNWK7LRAhVP9W7xxM-StJEkk') 
