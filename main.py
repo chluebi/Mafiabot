fcdd61af71816645be9f60b7b9a8ed5d22c6e559
celloboiErnest <ernestl8502@gmal.com>
2019-01-05 11:41:03 -0800

    dblUpdate

--- main.py
+++ main.py
@@ -5,21 +5,30 @@ import json
 import os
 import random
 import traceback
-bot = commands.Bot(command_prefix='/')
+import MAFIA.dbl as dblthing
+bot = commands.Bot(command_prefix='m.')
 bot.remove_command('help')
 
 extensions = ['mafia']
 players = {}
 @bot.event
 async def on_ready():
-    await bot.change_presence(game = discord.Game(name="/helpM", type=1))
+    dblthing.DiscordBotsOrgAPI(bot)
+    await bot.change_presence(game = discord.Game(name="m.help", type=1))
     print("I'm in")
-
 
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
         await bot.process_commands(message)
+
+@bot.event
+async def on_server_join(server):
+    print ("Joined server {}".format(server.name))
+
+@bot.event
+async def on_server_remove(server):
+    print ("Left server {}".format(server.name))
 if __name__ == '__main__':
     for extension in extensions:
         try:
